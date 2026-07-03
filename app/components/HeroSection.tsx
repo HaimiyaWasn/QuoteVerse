ab9f8db9c7e1896a3f4bccc3c9459b759c205fa0
Wasn <116284029+HaimiyaWasn@users.noreply.github.com>
2026-07-03 16:15:14 +0700

    Update code

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+export default function HeroSection() {
+  return (
+    <section className="min-h-screen bg-[#FFF8F0] text-[#2D241D]">
+      <header>
+        <h1>
+          QuoteVerse
+        </h1>
+      </header>
+    </section>
+  );
+}
